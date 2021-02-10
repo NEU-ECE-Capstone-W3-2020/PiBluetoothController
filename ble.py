87b094a892850dd24721aebc4d2654d863c6dde6
Nick Craffey <craffeyni@gmail.com>
2021-02-09 20:16:51 -0500

    Service UUIDs discovered

--- ble.py
+++ ble.py
@@ -1,7 +1,30 @@
-from bluepy import btle
-print "Connecting..."
-dev = btle.Peripheral("D7:3D:77:BF:DB:B3") 
-print "Services..."
-for svc in dev.services:
-    print str(svc)
+# from bluepy import btle
+# print "Connecting..."
 
+
+# print "Scanning for beacons..."
+
+
+# device = btle.Peripheral("D7:3D:77:BF:DB:B3", btle.ADDR_TYPE_RANDOM)
+# print "Services..."
+# for service in device.services:
+#     print str(service.uuid)
+from bluepy.btle import Scanner, DefaultDelegate
+
+class ScanDelegate(DefaultDelegate):
+    def __init__(self):
+        DefaultDelegate.__init__(self)
+
+    def handleDiscovery(self, dev, isNewDev, isNewData):
+        if isNewDev:
+            print "Discovered device", dev.addr
+        elif isNewData:
+            print "Received new data from", dev.addr
+
+scanner = Scanner().withDelegate(ScanDelegate())
+devices = scanner.scan(10.0)
+
+for dev in devices:
+    print "Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi)
+    for (adtype, desc, value) in dev.getScanData():
+        print "  %s = %s" % (desc, value)
