1e13617fc2495853e4723faea796d6d9df6acac0
Nick Craffey <craffeyni@gmail.com>
2021-02-10 01:38:28 +0000

    Filtering discovery for *our* device

--- ble.py
+++ ble.py
@@ -1,24 +1,27 @@
-from bluepy.btle import Scanner, DefaultDelegate, Peripheral, UUID
+from bluepy import btle
 
 nordic_uuid="6e400001-b5a3-f393-e0a9-e50e24dcca9e"
 
-class ScanDelegate(DefaultDelegate):
+class ScanDelegate(btle.DefaultDelegate):
     def __init__(self):
-        DefaultDelegate.__init__(self)
+        btle.DefaultDelegate.__init__(self)
 
     def handleDiscovery(self, dev, isNewDev, isNewData):
         if isNewDev:
             print "Discovered device", dev.addr
-            test_dev = btle.Peripheral(device.addr, btle.ADDR_TYPE_RANDOM)
-            for service in test_dev.services:
-            	print str(service.uuid)
-            	if service.uuid.getCommonName() == nordic_uuid:
-            		print("AND BINGO WAS HIS NAMO!!!")
+            try:
+                test_dev = btle.Peripheral(dev.addr, btle.ADDR_TYPE_RANDOM)
+                for service in test_dev.services:
+            	    print str(service.uuid)
+            	    if service.uuid.getCommonName() == nordic_uuid:
+            		print "AND BINGO WAS HIS NAMO!!!"
             		exit()
+            except:
+                print "Not our device !!"
         elif isNewData:
             print "Received new data from", dev.addr
 
-scanner = Scanner().withDelegate(ScanDelegate())
+scanner = btle.Scanner().withDelegate(ScanDelegate())
 devices = scanner.scan(10.0)
 
 for dev in devices:
