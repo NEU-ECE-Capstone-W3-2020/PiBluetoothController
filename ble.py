fe40761ec41f98e38b330554f66da32d9bb2e8cf
Nick Craffey <craffeyni@gmail.com>
2021-02-17 16:58:50 +0000

    Read hello world from beacon

--- ble.py
+++ ble.py
@@ -1,6 +1,7 @@
 from bluepy import btle
 
 nordic_uuid="6e400001-b5a3-f393-e0a9-e50e24dcca9e"
+device_mac=""
 
 class ScanDelegate(btle.DefaultDelegate):
     def __init__(self):
@@ -9,13 +10,31 @@ class ScanDelegate(btle.DefaultDelegate):
     def handleDiscovery(self, dev, isNewDev, isNewData):
         if isNewDev:
             print "Discovered device", dev.addr
-            for (adtype, desc, value) in dev.getScanData():
-                if value == nordic_uuid:
-                    print "DISCOVERED BLE BEACON!"
-                    print "  %s = %s" % (desc, value)
         elif isNewData:
             print "Received new data from", dev.addr
 
 scanner = btle.Scanner().withDelegate(ScanDelegate())
-devices = scanner.scan()
+devices = scanner.scan(1.0)
 
+for dev in devices:
+    for (adtype, desc, value) in dev.getScanData():
+        if value == nordic_uuid:
+            print "Discovered capstone device!"
+            device_mac = dev.addr
+            print device_mac
+            print dev.addr
+
+
+print "Device mac!", device_mac
+p = btle.Peripheral(device_mac, "random")
+
+try:
+    service = p.getServiceByUUID(nordic_uuid)
+    characteristics = service.getCharacteristics()
+    for c in characteristics:
+        print c.propertiesToString()
+        if (c.supportsRead()):
+            v = c.read()
+            print v
+finally:
+    p.disconnect()
