9708d3aba7038291c8be279c64ee1a797b5d6741
Nick Craffey <craffeyni@gmail.com>
2021-02-24 13:04:28 -0500

    Looping?

--- ble.py
+++ ble.py
@@ -1,33 +1,20 @@
-import struct
 from bluepy.btle import *
-
-# callback class
-class MyDelegate(DefaultDelegate):
-    def __init__(self):
-        DefaultDelegate.__init__(self)
-
-    def handleNotification(self, cHandle, data):
-        print(data)
 
 # connect to device
 per = Peripheral("d7:3d:77:bf:db:b3", "random")
 notify_uuid = '6e400003-b5a3-f393-e0a9-e50e24dcca9e'
 
-try:
-    # set callback for notifications
-    per.setDelegate(MyDelegate())
+last_str = ""
 
-    # enable notification
-    # setup_data = b"\x01\x00"
-    # notify = per.getCharacteristics(uuid=notify_uuid)[0]
-    # notify_handle = notify.getHandle() + 1
-    # write 1 to the notify characteristic to subscribe to notifications?
-    # per.writeCharacteristic(notify_handle, setup_data, withResponse=True)
-    
-    for c in per.getCharacteristics():
-        # print(c.propertiesToString())
-        if "READ" in c.propertiesToString():
-            if c.uuid == notify_uuid:
-                print(c.read().decode("utf-8"))
-finally:
-    per.disconnect()
+while True:
+    try:
+        # set callback for notifications
+        per.setDelegate(MyDelegate())
+
+        for c in per.getCharacteristics():
+            # print(c.propertiesToString())
+            if "READ" in c.propertiesToString():
+                if c.uuid == notify_uuid:
+                    cur_str = c.read().decode("utf-8")
+                    if cur_str != last_str:
+                        print(cur_str)
